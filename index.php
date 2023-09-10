1b2693b3be31d7aa050092b73c538d413c157410
yamini117 <parepalliyamini@gmail.com>
2023-09-10 16:54:01 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<? php include_once("home.html"); ?>
+<? php include_once("about.html"); ?>
+<? php include_once("contact.html"); ?>
+<? php include_once("b1.jpg"); ?>
+<? php include_once("b3.jpg"); ?>
+<? php include_once("c2.jpg"); ?>
